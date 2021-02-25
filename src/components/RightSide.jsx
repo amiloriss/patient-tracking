ffa5280910cddba46df53a00c5497efceabe611d
Georgii Tolparov <amiloriss@Georgiis-MacBook-Air.local>
2021-02-25 12:12:09 +0300

    add right side component

---
+++ src/components/RightSide.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Paper } from '@material-ui/core';
+
+const RightSide = () => {
+    return (
+        <Paper style={{ padding: '10px' }} square>
+            Right side
+        </Paper>
+    );
+};
+
+export default RightSide;
