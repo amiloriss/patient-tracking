56c90eb8a048c491b85c4cf94587f58a1d83fbdd
Georgii Tolparov <amiloriss@Georgiis-MacBook-Air.local>
2021-02-25 12:11:58 +0300

    add left side component

---
+++ src/components/LeftSide.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Paper } from '@material-ui/core';
+
+const LeftSide = () => {
+    return (
+        <Paper style={{ padding: '10px' }} square>
+            Left side
+        </Paper>
+    );
+};
+
+export default LeftSide;
