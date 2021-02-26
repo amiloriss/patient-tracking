c8e1d7eaa1201bf4c6e8479045a55a451620edee
Georgii Tolparov <amiloriss@Georgiis-MacBook-Air.local>
2021-02-26 11:34:35 +0300

    add buttons for selectind patient category

--- src/components/LeftSide.jsx
+++ src/components/LeftSide.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import { Paper } from '@material-ui/core';
 
 const LeftSide = () => {
-    return (
-        <Paper style={{ padding: '10px' }} square>
-            Left side
-        </Paper>
-    );
+    return <Paper square>Left side</Paper>;
 };
 
 export default LeftSide;
--- src/components/RightSide.jsx
+++ src/components/RightSide.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Paper } from '@material-ui/core';
 
 const RightSide = () => {
+    const [display, setDisplay] = useState('present');
     return (
-        <Paper style={{ padding: '10px' }} square>
-            Right side
+        <Paper square>
+            <div style={{ backgroundColor: '#f5f5f5' }}>
+                <button
+                    onClick={() => setDisplay('present')}
+                    style={btnStyle}
+                    className={display === 'present' && 'btn-selected'}>
+                    присутствуют
+                </button>
+                <button
+                    onClick={() => setDisplay('quiting')}
+                    style={btnStyle}
+                    className={display === 'quiting' && 'btn-selected'}>
+                    выбывшие
+                </button>
+            </div>
+            parp
         </Paper>
     );
+};
+
+const btnStyle = {
+    border: 'none',
+    fontSize: '16px',
+    fontWeight: '100',
+    textTransform: 'uppercase',
+    padding: '0 5px',
+    cursor: 'pointer',
+    padding: '10px',
+    backgroundColor: 'initial',
 };
 
 export default RightSide;
